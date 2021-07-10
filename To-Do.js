0124bcf1997e2c3b99bf8c98472c23c11aba824c
Bhuvaneswari Vimalraj <sgbhuvanait@gmail.com>
2021-07-10 17:41:08 +0800

    added by list

--- To-Do.js
+++ To-Do.js
@@ -1,32 +1,70 @@
+//Elements
 let clear = document.querySelector(".clear");
 let dateElement = document.getElementById("date");
-
 let list = document.getElementById("list");
-function addToDo(toDo) {
-  let text = `
-    <li class="item">
+let input = document.getElementById("input");
+let addBtnIcon = document.getElementById("add-btn-icon");
+
+//Classes
+let check = "bi bi-check-circle-fill";
+let uncheck = "bi bi-circle";
+let line_through = "lineThrough";
+
+//date
+let options = { weekday: "long", month: "short", day: "numeric" };
+let today = new Date();
+dateElement.innerHTML = today.toLocaleDateString("en-US", options);
+
+function addToDo() {
+  let todo = input.value;
+  if (!todo) {
+    return;
+  }
+  // if (trash) {
+  //   return;
+  // }
+  // let done = done ? check : uncheck;
+  // let line = done ? line_through : "";
+
+  let item = `
+    <li>
       <div class="row">
-      <div class="col-1"><i class="bi bi-circle" job="complete"></i></div>
-      <div class="col"><span class="text">${toDo}</span></div>
-      <div class="col-1"><i class="bi bi-basket" job="delete"></i></div>
+        <div class="col">
+          <i class ="fa fa-circle-thin co" job ="complete" id ="0"></i>
+          <span class="ps-2">${todo}</span>
+        </div>
+      
+        <div class="col text-end">
+          <i class ="fa fa-trash-o de" job="delete" id ="0"></i>
+        </div>
       </div>
     </li>`;
   let position = "beforeend";
 
-  list.insertAdjacentHTML(position, text);
+  list.insertAdjacentHTML(position, item);
 }
-addToDo("Tomato");
-addToDo("Onion");
-addToDo("Green Chilli");
+// addToDo("Tomato");
+// addToDo("Onion");
+// addToDo("Green Chilli");
+// addToDo("Drink Coffee");
 
-let input = document.getElementById("input");
+//add an item
+input.addEventListener("keyup", function (event) {
+  if (event.key === "Enter") {
+    addToDo();
+    // list.push({
+    //         name: todo,
+    //         id: id,
+    //         done: false,
+    //         trash: false,
+    //       });
 
-document.addEventListener("keyup", function (event) {
-  if (event.keycode == 13) {
-    let todo = input.Value;
-    if (todo) {
-      addToDo(todo);
-    }
-    input.value = "";
+    //     input.value = "";
+    //     id++;
   }
+});
+
+addBtnIcon.addEventListener("click", function (event) {
+  event.preventDefault();
+  addToDo();
 });
